8630663db9db8adf4a6d6abf993e463bf813edc3
Stephane Lafontaine <stephane@medqigong.ca>
2018-08-07 15:06:19 -0400

    Added seed and article model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |article|
+  new_article = Article.new(
+    title: Faker::Dune.character,
+    content: Faker::HarryPotter.quote
+    ) 
+  new_article.save 
+end
+
